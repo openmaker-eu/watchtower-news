b414de2c9f3a0f6d2d88bced96c211d73ee22878
Enis Simsar <enisimsar@gmail.com>
2018-11-22 11:10:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+eval $(cat .env | sed 's/^/export /')
+
+echo "$(whoami)"
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
 docker-compose down
 docker-compose up -d --scale rq-worker=$(grep WORKER_NUMBER .env | cut -d '=' -f2)
